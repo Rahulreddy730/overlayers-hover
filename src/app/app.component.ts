f8a9e84958cb7f00f23bcb83cdab0ffb92f91cba
rahulreddy730 <easwerrahulreddy@gmail.com>
2023-03-27 12:14:45 +0530

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import Point from 'ol/geom/Point';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Icon, Style } from 'ol/style';
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat } from 'ol/proj';
 import Overlay from 'ol/Overlay';
-import File from '../assets/sample.json';
+import { toStringHDMS } from 'ol/coordinate';
 
 @Component({
   selector: 'app-root',
@@ -22,8 +22,12 @@ export class AppComponent {
 
 
   map!: Map;
+  // overlay: any;
   vectorLayer:any;
   httpClient!: HttpClient;
+  closer: any;
+  content: any;
+  container: any;
 
   constructor(http: HttpClient) {
     this.httpClient = http;
@@ -34,6 +38,25 @@ export class AppComponent {
   }
 
   initMap() {
+
+    this. container = document.getElementById('popup');
+    this.content = document.getElementById('popup-content');
+    this.closer = document.getElementById('popup-closer');
+
+    const overlay = new Overlay({
+      element: this.container,
+      autoPan: {
+        animation: {
+          duration: 250,
+        },
+      },
+    });
+
+    this.closer.onclick = () => {
+      overlay.setPosition(undefined);
+      this.closer.blur();
+      return false;
+    };
     const vectorSource = new VectorSource();
     this.vectorLayer = new VectorLayer({
       source: vectorSource,
@@ -48,8 +71,8 @@ export class AppComponent {
         this.vectorLayer,
       ],
       view: new View({
-        center: fromLonLat([-30, 80]),
-        zoom: 2,
+        center: fromLonLat([-90, 33]),
+        zoom: 5,
       }),
     });
 
@@ -57,8 +80,9 @@ export class AppComponent {
       data.forEach((marker: { Lon: number; Lat: number; Name: string; })=> {
         const feature = new Feature({
           geometry:new Point(fromLonLat([marker.Lon,marker.Lat])),
+          Name:marker.Name
         });
-       
+
         const iconStyle = new Style({
           image:new Icon({
             anchor:[0.5,1],
@@ -66,37 +90,25 @@ export class AppComponent {
           }),
         });
         feature.setStyle(iconStyle);
-        feature.set('name', marker.Name);
-        vectorSource.addFeature(feature)
-        const tooltip = document.createElement('div');
-        tooltip.classList.add('tooltip');
-        tooltip.innerHTML = marker.Name;
 
-        const overlay = new Overlay({
-          element:tooltip,
-          offset:[0,-21],
-          positioning:'bottom-center'
-        });
+        vectorSource.addFeature(feature)
 
         this.map.addOverlay(overlay);
-        this.map.on('pointermove', (event) => {
-          this.map.getTargetElement().style.cursor = 'default';
-          const feature = this.map.forEachFeatureAtPixel(event.pixel, (ft) => ft);
-          if (feature) {
-            const tooltip = feature.get('name');
-            if (tooltip) {
-              this.map.getTargetElement().title = tooltip;
-              this.map.getTargetElement().style.cursor = 'pointer';
-            }
-          } else {
-            this.map.getTargetElement().title = 'Name';
-          }
+        this.map.on('singleclick', (event) => {
+
+          const coordinate = event.coordinate;
+          const hdms = toStringHDMS(toLonLat(coordinate));
+          
+
+          this.map.forEachFeatureAtPixel(event.pixel, (feature) => {
+            const locationName = feature.get('Name');
+            // console.log(locationName); 
+            this.content.innerHTML = locationName + '<br/><br/><code>' + hdms + '</code>';
+            overlay.setPosition(coordinate);
+          });
         });
-        // feature.on('click', (event) =>{
-        //   overlay.setPosition(event.target.map)
-        // })
       });
-      console.log(data)
+      // console.log(data)
     });
   }
 }
